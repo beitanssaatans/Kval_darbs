dd05fe9893ad83953882fad2366e2344f7cf5faa
Krisjanis <krisjanis100@gmail.com>
2019-05-23 00:02:04 +0300

    summery arī pielikts attēls

--- my-app/src/components/Posts/PostsSummary.js
+++ my-app/src/components/Posts/PostsSummary.js
@@ -5,6 +5,7 @@ const PostsSummary = ({post}) => {
     return(
         <div className="card z-depth-0 project-summary">
         <div className="card-content grey-text text-darken-3">
+            <img src={ post.image }></img>
             <span className="card-title">{post.title}</span>
             <p>Posted by {post.authorFirstName} {post.authorLastName}</p>
             <p className="grey-text">{moment(post.createdAt.toDate()).calendar()}</p>
